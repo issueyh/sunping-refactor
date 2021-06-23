804d43723cd766cd9fdb4503d2143539f6fa5183
issueyh <issueyh12921@gmail.com>
2021-06-23 16:47:11 +0800

    feat: add navbar

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const port = 2725
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
     res.render('index')
 })

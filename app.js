8a2d858255f5668ff20a0dc01bdd49b60bd07b7f
issueyh <issueyh12921@gmail.com>
2021-06-23 16:53:12 +0800

    feat: use body-parser

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
     res.render('index')

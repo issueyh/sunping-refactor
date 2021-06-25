bbe6984c073e6756dd18ce451799e0c53962e346
issueyh <issueyh12921@gmail.com>
2021-06-25 17:06:48 +0800

    refactor: add partials

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const exphbs = require('express-handlebars')
 const app = express()
 const port = 2725
 
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.engine('hbs', exphbs({ 
+    defaultLayout: 'main',
+    extname: '.hbs',
+    partialsDir: 'views/partials'
+}))
 app.set('view engine', 'hbs')
 
 app.use(express.static('public'))
